f609502b8930cf16a7e7f22ee33c7a0d2abf7224
mahardikakdenie <kucingemas744@gmail.com>
2026-04-03 22:52:22 +0700

    refactor: rebuild dashboard page into bento layout

--- src/views/dashboard/UserDashboard.tsx
+++ src/views/dashboard/UserDashboard.tsx
@@ -6,23 +6,36 @@ import { RecentAttendance } from "@/components/dashboard-user/RecentAttendance";
 
 export default function UserDashboardPage() {
   return (
-    <div className="space-y-6">
+    <div className="flex flex-col gap-6 md:gap-8 w-full max-w-7xl mx-auto pb-10">
+      
+      {/* 1. Header Section */}
+      <section>
+        <GreetingCard />
+      </section>
 
-      <GreetingCard />
-
-      <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
-
-        <div className="lg:col-span-2">
-          <ClockCard />
+      {/* 2. Main Bento Grid Section */}
+      {/* Menggunakan grid 12 kolom untuk kontrol proporsi yang lebih presisi */}
+      <section className="grid grid-cols-1 lg:grid-cols-12 gap-6 md:gap-8">
+        
+        {/* Kolom Kiri: Fokus Utama (Clock Action) - Proporsi lebih besar (7/12) */}
+        <div className="lg:col-span-7 xl:col-span-8 flex flex-col">
+          <div className="h-full w-full flex items-center">
+            <ClockCard />
+          </div>
         </div>
 
-        <div className="lg:col-span-2 flex flex-col gap-6">
+        {/* Kolom Kanan: Ringkasan & Info - Proporsi lebih kecil (5/12) */}
+        <div className="lg:col-span-5 xl:col-span-4 flex flex-col gap-6 md:gap-8">
           <TodayStatusCard />
           <QuickInfoCard />
         </div>
-      </div>
+        
+      </section>
 
-      <RecentAttendance />
+      {/* 3. Data/Table Section */}
+      <section className="mt-2">
+        <RecentAttendance />
+      </section>
 
     </div>
   );
