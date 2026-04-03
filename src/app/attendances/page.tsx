af9a8d008d4f0c769cacbefe6c09435f2a674e80
mahardikakdenie <kucingemas744@gmail.com>
2026-04-03 22:52:16 +0700

    refactor: remove top-level attendances page from app root

--- src/app/attendances/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import AttendancesView from "@/views/attendances/Index";
-
-const AttendancePage: React.FC = () => {
-    return <AttendancesView />;
-};
-
-export default AttendancePage;
