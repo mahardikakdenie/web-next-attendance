032a2e1a8bece5ea3e89ae9d870a8dd2174418ea
mahardikakdenie <kucingemas744@gmail.com>
2026-04-03 22:52:21 +0700

    feat: expand auth service with session helpers

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,9 +1,30 @@
+// src/services/auth.service.ts
 import { api } from "@/lib/axios";
 
-export const login = async (payload: {
+export const loginAPI = async (payload: {
   email: string;
   password: string;
 }) => {
-  const res = await api.post("/auth/login", payload);
+  const res = await api.post("/v1/auth/login", payload, {
+    withCredentials: true,
+  });
+  return res.data;
+};
+
+export const getMeAPI = async () => {
+  const res = await api.get("/v1/users/me", {
+    withCredentials: true,
+  });
+  return res.data;
+};
+
+export const logoutAPI = async () => {
+  const res = await api.post(
+    "/v1/auth/logout",
+    {},
+    {
+      withCredentials: true,
+    }
+  );
   return res.data;
 };
