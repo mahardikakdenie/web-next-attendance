1428b1e951eac6c2616630782e9a9780edd05daa
mahardikakdenie <kucingemas744@gmail.com>
2026-04-04 14:16:15 +0700

    feat(greeting): implementation greeting with realtime Token and current user logged

--- src/components/dashboard-user/GreetingCard.tsx
+++ src/components/dashboard-user/GreetingCard.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { useAuthStore } from "@/store/auth.store";
 import dayjs from "dayjs";
 
 export default function GreetingCard() {
+  const {user} = useAuthStore();
   const hour = dayjs().hour();
 
   const greeting =
@@ -14,8 +16,8 @@ export default function GreetingCard() {
 
   return (
     <div className="bg-white/80 backdrop-blur rounded-2xl p-6">
-      <h1 className="text-xl font-semibold text-slate-900">
-        {greeting}, Mahardika 👋
+      <h1 className="text-xl font-semibold text-slate-900 capitalize">
+        {greeting}, {user?.name} 👋
       </h1>
       <p className="text-sm text-slate-500 mt-1">
         {dayjs().format("dddd, DD MMMM YYYY")}
