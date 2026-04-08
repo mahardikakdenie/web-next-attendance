d7d774bad07213f04ba4f143a1c06302185b6812
mahardikakdenie <kucingemas744@gmail.com>
2026-04-08 23:25:28 +0700

    chore: update meta data title header website for information Page

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -1,6 +1,11 @@
-'use client';
 import UserDashboardPage from "@/views/dashboard/UserDashboard";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard | Attendance Management",
+  description: "Employee attendance dashboard.",
+};
 
 export default function Page() {
-  return <UserDashboardPage />
+  return <UserDashboardPage />;
 }
