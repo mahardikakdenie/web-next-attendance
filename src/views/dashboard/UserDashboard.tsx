54243bad4f7308b5910bcfbff9a59c0ad4a7f926
mahardikakdenie <kucingemas744@gmail.com>
2026-04-08 23:39:34 +0700

    feat(user-dashboard): adjustment new layouting for user Dashboard Managements

--- src/views/dashboard/UserDashboard.tsx
+++ src/views/dashboard/UserDashboard.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import GreetingCard from "@/components/dashboard-user/GreetingCard";
 import ClockCard from "@/components/dashboard-user/ClockCard";
 import TodayStatusCard from "@/components/dashboard-user/TodayStatusCard";
 import { QuickInfoCard } from "@/components/dashboard-user/QuickInfoCard";
 import { RecentAttendance } from "@/components/dashboard-user/RecentAttendance";
+import { OvertimeRequestCard } from "@/components/dashboard-user/OvertimeRequestCard";
+import { RecentActivityCard } from "@/components/dashboard-user/RecentlyActivity";
 
 export default function UserDashboardPage() {
   return (
@@ -14,26 +18,35 @@ export default function UserDashboardPage() {
       </section>
 
       {/* 2. Main Bento Grid Section */}
-      {/* Menggunakan grid 12 kolom untuk kontrol proporsi yang lebih presisi */}
-      <section className="grid grid-cols-1 lg:grid-cols-12 gap-6 md:gap-8">
+      <section className="flex flex-col gap-6 lg:gap-8">
         
-        {/* Kolom Kiri: Fokus Utama (Clock Action) - Proporsi lebih besar (7/12) */}
-        <div className="lg:col-span-7 xl:col-span-8 flex flex-col">
-          <div className="h-full w-full flex items-center">
+        {/* Row 1: Primary Actions (Clock Action & Overtime Request) */}
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 lg:gap-8 items-stretch">
+          <div className="lg:col-span-7 xl:col-span-8 h-full">
             <ClockCard />
+          </div>
+          <div className="lg:col-span-5 xl:col-span-4 h-full">
+            <OvertimeRequestCard />
           </div>
         </div>
 
-        {/* Kolom Kanan: Ringkasan & Info - Proporsi lebih kecil (5/12) */}
-        <div className="lg:col-span-5 xl:col-span-4 flex flex-col gap-6 md:gap-8">
-          <TodayStatusCard />
-          <QuickInfoCard />
+        {/* Row 2: Information & Summary (Bento Sub-items) */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
+          <div className="flex flex-col">
+            <TodayStatusCard />
+          </div>
+          <div className="flex flex-col">
+            <QuickInfoCard />
+          </div>
+          <div className="flex flex-col">
+            <RecentActivityCard />
+          </div>
         </div>
         
       </section>
 
       {/* 3. Data/Table Section */}
-      <section className="mt-2">
+      <section className="mt-4">
         <RecentAttendance />
       </section>
 
